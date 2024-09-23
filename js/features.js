a6afeefdd3a419a822c9ec9ab474eebd5a3550c0
Sushanta65 <sushanta9119@gmail.com>
2024-09-23 22:37:37 +0600

    modal and alert done

--- js/features.js
+++ js/features.js
@@ -1,8 +1,17 @@
 
-document.getElementById('donation-btn').addEventListener('click', function(){
-    
-    showSectionById('donation-section')
-})
-document.getElementById('donate-history-btn').addEventListener('click', function(){
-    showSectionById('history-section')
-})
+
+//Donation Section and History Section Switching Function
+
+function showSectionById(id) {
+    document.getElementById("donation-section").classList.add("hidden");
+    document.getElementById("history-section").classList.add("hidden");
+    document.getElementById(id).classList.remove("hidden");
+  }
+
+
+// Input Value
+function getInputValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputValueNumber = parseFloat(inputValue)
+    return inputValueNumber;
+}
--- js/script.js
+++ js/script.js
@@ -1,53 +1,56 @@
-document
-  .getElementById("donate-noakhali-btn")
-  .addEventListener("click", function () {
-    let accountBalance = parseFloat(
-      document.getElementById("main-balance").innerText
-    );
-    let noakhaliDonate = parseFloat(
-      document.getElementById("noakhali-donate-amount").innerText
-    );
+    
+    
+    
+      //Donate Section and History Section Switching By Calling showSectingById function.
+
+  document.getElementById('donation-btn').addEventListener('click', function(){
+    showSectionById('donation-section')
+})
+
+  document.getElementById('donate-history-btn').addEventListener('click', function(){
+    showSectionById('history-section')
+})
+
+    // Noakhali Flood Donation Section Start Here
+
+    document.getElementById("donate-noakhali-btn").addEventListener("click", function () {
+
+    let accountBalance = parseFloat(document.getElementById("main-balance").innerText);
+    let noakhaliDonate = parseFloat(document.getElementById("noakhali-donate-amount").innerText);
 
     //get the input value
-    let donateAmount = parseFloat(
-      document.getElementById("noakhali-donate-input").value
-    );
+    let donateAmount = getInputValueById('noakhali-donate-input');
 
-    if (
-      !isNaN(donateAmount) &&
-      donateAmount > 0 &&
-      accountBalance >= donateAmount
-    ) {
+    if (!isNaN(donateAmount) && donateAmount > 0 && accountBalance >= donateAmount) {
+
       const newBalance = accountBalance - donateAmount;
       const donatedAmount = noakhaliDonate + donateAmount;
 
       document.getElementById("main-balance").innerText = newBalance;
-      document.getElementById("noakhali-donate-amount").innerText =
-        donatedAmount;
+      document.getElementById("noakhali-donate-amount").innerText = donatedAmount;
 
 
-
-
+        //Adding transaction to history section
         const transactionHistory = document.createElement("div");
         const donateTitle = document.getElementById('noakhali-donate-titile').innerText;
 
         transactionHistory.innerHTML = `
-       <div class="w-full border-2 border-gray-300 m-5 p-3 rounded-md">
-        <h2 class="text-xl font-bold ">${donateAmount} Taka is donated for ${donateTitle}</h2>
-        <small>${new Date()}</small>
-      </div>
+        <div class="w-full border-2 border-gray-300 m-5 p-3 rounded-md">
+            <h2 class="text-xl font-bold ">${donateAmount} Taka is donated for ${donateTitle}</h2>
+            <small>${new Date()}</small>
+        </div>
        `;
 
         document.getElementById('history-section').appendChild(transactionHistory)
+
+      // Show Modal 
+        document.getElementById('my_modal_1').showModal();
 
     } else {
       alert("Please Enter a valid number.");
     }
   });
 
-function showSectionById(id) {
-  document.getElementById("donation-section").classList.add("hidden");
-  document.getElementById("history-section").classList.add("hidden");
+      // Noakhali Flood Donation Section End Here
 
-  document.getElementById(id).classList.remove("hidden");
-}
+
