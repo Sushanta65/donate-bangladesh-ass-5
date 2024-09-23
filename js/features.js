311dcd0ba3517bedee9f4e2f8384fa2405c98b26
Sushanta65 <sushanta9119@gmail.com>
2024-09-23 09:10:07 +0600

    toggle section done

---
+++ js/features.js
@@ -0,0 +1,8 @@
+
+document.getElementById('donation-btn').addEventListener('click', function(){
+    
+    showSectionById('donation-section')
+})
+document.getElementById('donate-history-btn').addEventListener('click', function(){
+    showSectionById('history-section')
+})
--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
-
-
-document.getElementById('donate-noakhali-btn').addEventListener('click', function(event){
-    let accountBalance = parseFloat(document.getElementById('main-balance').innerText)
-    let noakhaliDonate = parseFloat(document.getElementById('noakhali-donate-amount').innerText)
-
-
-
+document
+  .getElementById("donate-noakhali-btn")
+  .addEventListener("click", function () {
+    let accountBalance = parseFloat(
+      document.getElementById("main-balance").innerText
+    );
+    let noakhaliDonate = parseFloat(
+      document.getElementById("noakhali-donate-amount").innerText
+    );
 
     //get the input value
-    let donateAmount = parseFloat(document.getElementById('noakhali-donate-input').value)
+    let donateAmount = parseFloat(
+      document.getElementById("noakhali-donate-input").value
+    );
 
-    if(!isNaN(donateAmount) && donateAmount > 0 && accountBalance > donateAmount){
-        // document.getElementById('donate-noakhali-btn').setAttribute('onclick', 'my_modal_1.showModal()')
-       const newBalance = accountBalance - donateAmount;
-       const donatedAmount = noakhaliDonate + donateAmount;
+    if (
+      !isNaN(donateAmount) &&
+      donateAmount > 0 &&
+      accountBalance >= donateAmount
+    ) {
+      const newBalance = accountBalance - donateAmount;
+      const donatedAmount = noakhaliDonate + donateAmount;
 
-       document.getElementById('main-balance').innerText = newBalance;
-       document.getElementById('noakhali-donate-amount').innerText = donatedAmount;
+      document.getElementById("main-balance").innerText = newBalance;
+      document.getElementById("noakhali-donate-amount").innerText =
+        donatedAmount;
 
-       
-        
-    }else{
-        // document.getElementById('donate-noakhali-btn').removeAttribute('onclick')
-        alert("Please Enter a valid number.")
-        
-        
+
+
+
+        const transactionHistory = document.createElement("div");
+        const donateTitle = document.getElementById('noakhali-donate-titile').innerText;
+
+        transactionHistory.innerHTML = `
+       <div class="w-full border-2 border-gray-300 m-5 p-3 rounded-md">
+        <h2 class="text-xl font-bold ">${donateAmount} Taka is donated for ${donateTitle}</h2>
+        <small>${new Date()}</small>
+      </div>
+       `;
+
+        document.getElementById('history-section').appendChild(transactionHistory)
+
+    } else {
+      alert("Please Enter a valid number.");
     }
-})
+  });
 
+function showSectionById(id) {
+  document.getElementById("donation-section").classList.add("hidden");
+  document.getElementById("history-section").classList.add("hidden");
 
+  document.getElementById(id).classList.remove("hidden");
+}
