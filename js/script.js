46903d7ea0fb5aa7f01c8db9f138879ed59159f6
Sushanta65 <sushanta9119@gmail.com>
2024-09-23 07:51:56 +0600

    Navbar and Donate, History Button Section Done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log("Hello Assignment-5");
+
+
+document.getElementById('donate-noakhali-btn').addEventListener('click', function(event){
+    let accountBalance = parseFloat(document.getElementById('main-balance').innerText)
+    let noakhaliDonate = parseFloat(document.getElementById('noakhali-donate-amount').innerText)
+
+
+
+
+    //get the input value
+    let donateAmount = parseFloat(document.getElementById('noakhali-donate-input').value)
+
+    if(!isNaN(donateAmount) && donateAmount > 0 && accountBalance > donateAmount){
+        // document.getElementById('donate-noakhali-btn').setAttribute('onclick', 'my_modal_1.showModal()')
+       const newBalance = accountBalance - donateAmount;
+       const donatedAmount = noakhaliDonate + donateAmount;
+
+       document.getElementById('main-balance').innerText = newBalance;
+       document.getElementById('noakhali-donate-amount').innerText = donatedAmount;
+
+       
+        
+    }else{
+        // document.getElementById('donate-noakhali-btn').removeAttribute('onclick')
+        alert("Please Enter a valid number.")
+        
+        
+    }
+})
+
+
